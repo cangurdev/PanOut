c88916edba5bcf943a472691274f15212a84c7f2
cvngur <cangur2398@gmail.com>
2021-04-28 17:05:24 +0300

    widgets file structure changed

--- lib/views/goal_form_view.dart
+++ lib/views/goal_form_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pan_out/widgets/bottom_navbar.dart';
-import 'package:pan_out/widgets/form_navbar.dart';
+import 'package:pan_out/widgets/bottom_navbar/bottom_navbar.dart';
+import 'package:pan_out/widgets/top_navbar/form_navbar.dart';
 
 class GoalForm extends StatelessWidget {
   @override
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 //import widgets
-import 'package:pan_out/widgets/Fab.dart';
-import 'package:pan_out/widgets/bottom_navbar.dart';
-import 'package:pan_out/widgets/home_navbar.dart';
+import 'package:pan_out/widgets/bottom_navbar/Fab.dart';
+import 'package:pan_out/widgets/bottom_navbar/bottom_navbar.dart';
+import 'package:pan_out/widgets/top_navbar/home_navbar.dart';
 
 class Home extends StatelessWidget {
   @override
